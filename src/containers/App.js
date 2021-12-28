70274e39cc5948d647f6f69638aabaa7220791a4
relieyanhilman <18219089@std.stei.itb.ac.id>
2021-12-28 11:23:27 +0800

    add robofriends file

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+// import {robots} from './robots.js';
+import SearchBox from '../components/SearchBox';
+import './app.css';
+import Scroll from "../components/Scroll"
+
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots : [],
+            searchFilter:''
+        }
+        
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchFilter: event.target.value})
+        // console.log(event.target.value);
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots : users}))
+        
+    }
+
+    render(){
+        const {robots, searchFilter} = this.state;
+        const filteredComponent = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchFilter.toLowerCase())
+        });
+        return (!robots.length)? <h1>Loading</h1> :
+            (
+                <>
+                <div className='tc'>
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                    <CardList robots={filteredComponent} />
+                    </Scroll>
+                </div>
+                </>
+            )
+    }
+    
+}
+
+export default App;
