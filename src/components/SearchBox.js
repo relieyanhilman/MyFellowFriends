bd61ec8c17fef9679760033e0c740cf319dd5b8f
relieyanhilman <relieyanhilman93@gmail.com>
2023-03-04 15:19:03 +0700

    change favicon

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './searchBox.css';
-const SearchBox = ({searchChange}) => {
-    return (
-        <>
-        <div className='pa2'>
-            <input
-            className='pa3 ba b--green bg-lightest-blue'
-            type='search' 
-            placeholder='search robots' 
-            onChange={searchChange}
+import React from "react";
+import "./searchBox.css";
+const SearchBox = ({ searchChange }) => {
+  return (
+    <>
+      <div className="pa2">
+        <input
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          placeholder="SEARCH FRIENDS"
+          onChange={searchChange}
         />
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default SearchBox;
